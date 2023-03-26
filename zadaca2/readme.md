308c7f6dab7e73bc059d7c6fbf98765b5fc4cb4d
Lejlana <lelabeganovic@gmail.com>
2023-03-26 13:54:07 +0200

    Izrada zadace

---
+++ zadaca2/readme.md
@@ -0,0 +1,19 @@
+ZADACA:
+- Napravite program koji na osnovu  novca u kasi i racuna izracuna koliko je kusur
+  na ispisu pokazati koliko bi bio kusur u navedenim apoenima sljedecim redosljedom (koristite int ).
+  20 KM
+  10 KM
+  2 KM
+  1 KM
+  USLOV. maximalna kolicina novca u kasi  ne moze biti vise od 100 km ukoliko je vise, program treba izbaciti poruku
+  “You have too much money” i naravno da racun mora biti veci od 0;
+  ukoliko je 0 i manje da ispise poruku “You didnt buy anything did you”. Ukoliko je manje od 0
+  dodati liniju “Did you maybe break something.”
+  NPR : Racun je 66 KM, novac je 100 KM
+  34 KM je ostalo sto bi znacilo
+  20 KM = 1
+  10 KM = 1
+  2  KM = 2
+  1  KM = 0;
+
+
--- zadaca2/src/Main.java
+++ zadaca2/src/Main.java
@@ -1,5 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int moneyInCashier = 100;
+        int bill = 66;
+
+        int bankNote1 = 20;
+        int bankNote2 = 10;
+        int bankNote3 = 2;
+        int bankNote4 = 1;
+
+        int change = moneyInCashier - bill;
+
+        System.out.println("The change is " + change);
+
+        int apoen20 = change / bankNote1;
+        System.out.println("20 KM = " + apoen20);
+
+        int apoen10 = (change - (apoen20 * bankNote1)) / bankNote2;
+        System.out.println("10 KM = " + apoen10);
+
+        int apoen2 = (change - ((apoen20 * bankNote1) + (apoen10 * bankNote2))) / bankNote3;
+        System.out.println("2 KM = " + apoen2);
+
+        int apoen1 = (change - ((apoen20 * bankNote1) + (apoen10 * bankNote2) + (apoen2 * bankNote3))) / bankNote4;
+        System.out.println("1 KM = " + apoen1);
+
+        if (moneyInCashier > 100) {
+            System.out.println("You have too much money!");
+        }
+        if (bill <= 0) {
+            System.out.println("You didn't buy anything, did you?");
+        }
+        if (bill < 0) {
+            System.out.println("Did you maybe break something.");
+
+
+        }
     }
+
 }
