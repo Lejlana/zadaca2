19c7d7cf78c3e9c8dc4fcfe94bfe128c20f0f776
Lejlana <lelabeganovic@gmail.com>
2023-03-26 21:38:59 +0200

    Izrada zadace

--- zadaca2/src/Main.java
+++ zadaca2/src/Main.java
@@ -11,19 +11,9 @@ public class Main {
 
         int change = moneyInCashier - bill;
 
-        System.out.println("The change is " + change);
+        System.out.println("");
 
-        int apoen20 = change / bankNote1;
-        System.out.println("20 KM = " + apoen20);
-
-        int apoen10 = (change - (apoen20 * bankNote1)) / bankNote2;
-        System.out.println("10 KM = " + apoen10);
-
-        int apoen2 = (change - ((apoen20 * bankNote1) + (apoen10 * bankNote2))) / bankNote3;
-        System.out.println("2 KM = " + apoen2);
-
-        int apoen1 = (change - ((apoen20 * bankNote1) + (apoen10 * bankNote2) + (apoen2 * bankNote3))) / bankNote4;
-        System.out.println("1 KM = " + apoen1);
+       //To be continued
 
         if (moneyInCashier > 100) {
             System.out.println("You have too much money!");
